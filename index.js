3fc670fa11c834eb69a4795e9d055cee4820fd9a
sahidul-11 <mdsahidulislam161113@gmail.com>
2024-08-17 11:16:41 +0600

    pagininations added

--- index.js
+++ index.js
@@ -27,6 +27,44 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+    const ProductsCollation=client.db("JOBTask").collection("products")
+
+
+// const page = parseInt(req.query.page) || 1; // Default to page 1
+        // const limit = parseInt(req.query.limit) || 10; // Default to 10 products per page
+    
+        // const skip = (page - 1) * limit; // Calculate the number of documents to skip
+    
+        // const total = await productCollection.countDocuments(); // Get total number of products
+        // const pages = Math.ceil(total / limit); // Calculate total pages
+    
+        // const products = await productCollection.find()
+        //     .skip(skip)
+        //     .limit(limit)
+        //     .toArray();
+    
+        // res.send({
+        //     products,
+        //     total,
+        //     page,
+        //     pages
+
+
+    app.get("/products",async(req ,res)=>{
+      const {currentPage}=req.query;
+      const limit = 9;
+      const skip = parseInt(currentPage) * limit;
+      const result =await ProductsCollation.find().skip(skip).limit(limit).toArray()
+      res.send(result)
+    });
+
+
+    app.get("/totalProducts",async(req,res)=>{
+      console.log("hit")
+      const count = await ProductsCollation.countDocuments()
+      res.send({count})
+      console.log(count)
+    });
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
